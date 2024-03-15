6f556106371f817ad8ceac2a71789a0cfc9ce168
lakshmi5382 <v.l.bharadwaja@gmail.com>
2024-03-15 16:49:44 +0100

    Initial commit

---
+++ src/main/java/com/jensens/mobileshop/JensensMobileRepairShopApplication.java
@@ -0,0 +1,13 @@
+package com.jensens.mobileshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JensensMobileRepairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JensensMobileRepairShopApplication.class, args);
+	}
+
+}
